f3e456c1aa1eca4884c98d0f13e46eafd7cf642e
vombat11 <gulyas.kata11@gmail.com>
2023-04-28 14:11:33 +0200

    corrected github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes['made-by']}>Made by: Katalin Rácz-Gulyás</div>
-      <div><a href="https://github.com/gulyaskata11?tab=repositories">Github page</a></div>
+      <div><a href="https://github.com/gulyaskata11/weather-app-react2.0">Github page</a></div>
     </footer>
   )
 }
